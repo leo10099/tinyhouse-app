f5a7b34096f42b3da3e6ef7cffa35308922083e5
leo10099 <leandrohanc@gmail.com>
2020-07-16 14:14:45 -0300

    Add spaces between typedefs

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -8,18 +8,22 @@ export const typeDefs = gql`
     checkIn: String!
     checkOut: String!
   }
+
   type Bookings {
     total: Int!
     result: [Booking!]!
   }
+
   enum ListingType {
     APARTMENT
     HOUSE
   }
+
   enum ListingsFilter {
     PRICE_LOW_TO_HIGH
     PRICE_HIGH_TO_LOW
   }
+
   type Listing {
     id: ID!
     title: String!
@@ -36,11 +40,13 @@ export const typeDefs = gql`
     price: Int!
     numOfGuests: Int!
   }
+
   type Listings {
     region: String
     total: Int!
     result: [Listing!]!
   }
+
   type Viewer {
     id: ID
     token: String
@@ -48,6 +54,7 @@ export const typeDefs = gql`
     hasWallet: Boolean
     didRequestGoogleInfo: Boolean!
   }
+
   type User {
     id: ID!
     name: String!
@@ -58,20 +65,18 @@ export const typeDefs = gql`
     bookings(limit: Int!, page: Int!): Bookings
     listings(limit: Int!, page: Int!): Listings!
   }
+
   input LogInInput {
     code: String!
   }
+
   type Query {
     authUrl: String!
     user(id: ID!): User!
     listing(id: ID!): Listing!
-    listings(
-      location: String
-      filter: ListingsFilter!
-      limit: Int!
-      page: Int!
-    ): Listings!
+    listings(filter: ListingsFilter!, limit: Int!, page: Int!): Listings!
   }
+
   type Mutation {
     logIn(input: LogInInput): Viewer!
     logOut: Viewer!
