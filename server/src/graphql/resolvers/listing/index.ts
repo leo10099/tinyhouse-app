e125ce74f24873d0e97f0214c1b99a33659d0741
leo10099 <leandrohanc@gmail.com>
2020-07-17 11:55:07 -0300

    Add google geocode. Add env example.

--- server/src/graphql/resolvers/listing/index.ts
+++ server/src/graphql/resolvers/listing/index.ts
@@ -2,6 +2,7 @@ import { IResolvers } from "apollo-server-express";
 import { Database, Listing, User } from "../../../lib/types";
 import { ListingArgs, ListingsFilter } from "./types";
 import { ObjectId } from "mongodb";
+import { Google } from "../../../lib/api";
 import { authorize } from "../../../lib/utils";
 import { Request } from "express";
 import {
@@ -9,22 +10,40 @@ import {
   ListingBookingsData,
   ListingsArgs,
   ListingsData,
+  ListingsQuery,
 } from "./types";
 
 export const listingResolvers: IResolvers = {
   Query: {
     listings: async (
       _root: undefined,
-      { filter, limit, page }: ListingsArgs,
+      { location, filter, limit, page }: ListingsArgs,
       { db }: { db: Database }
     ): Promise<ListingsData> => {
       try {
+        const query: ListingsQuery = {};
         const data: ListingsData = {
+          region: null,
           total: 0,
           result: [],
         };
 
-        let cursor = await db.listings.find();
+        if (location) {
+          const { country, admin, city } = await Google.geocode(location);
+          if (city) query.city = city;
+          if (admin) query.admin = admin;
+          if (country) {
+            query.country = country;
+          } else {
+            throw new Error("Couldn't find country");
+          }
+
+          const cityText = city ? `${city}, ` : "";
+          const adminText = admin ? `${admin}, ` : "";
+          data.region = `${cityText}${adminText}$`;
+        }
+
+        let cursor = await db.listings.find(query);
 
         if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
           cursor = cursor.sort({ price: 1 });
--- server/src/lib/api/Google.ts
+++ server/src/lib/api/Google.ts
@@ -1,10 +1,22 @@
 import { google } from "googleapis";
+import { createClient } from "@google/maps";
 
 const auth = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   `${process.env.PUBLIC_URL}/login`
 );
+
+interface ParsedAddress {
+  country: string | null;
+  admin: string | null;
+  city: string | null;
+}
+
+const maps = createClient({
+  key: `${process.env.GOOGLE_GEOCODING_API_KEY}`,
+  Promise: Promise,
+});
 
 export const Google = {
   authUrl: auth.generateAuthUrl({
@@ -27,5 +39,41 @@ export const Google = {
     return {
       user: data,
     };
+  },
+  geocode: async (address: string): Promise<ParsedAddress> => {
+    const res = await maps.geocode({ address }).asPromise();
+
+    if (res.status < 200 || res.status > 299) {
+      throw new Error("Failed to geocode address");
+    }
+
+    let country = null;
+    let admin = null; //state
+    let city = null;
+
+    //parse data
+    if (res && res.json.results && res.json.results.length > 0) {
+      res.json.results[0].address_components.forEach((address) => {
+        //check if there is match on country from data received
+        if (address.types.includes("country")) {
+          country = address.long_name;
+        }
+
+        //check if there is match on state from data received
+        if (address.types.includes("administrative_area_level_1")) {
+          admin = address.long_name;
+        }
+
+        //check if there is match on city from data received
+        if (
+          address.types.includes("locality") ||
+          address.types.includes("postal_town")
+        ) {
+          city = address.long_name;
+        }
+      });
+    }
+
+    return { country, admin, city };
   },
 };
